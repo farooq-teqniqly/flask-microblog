01508c28ef183755aa90aef7878c72e13369608a
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-05-03 16:11:24 -0700

    Welcome and login pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from config import Config, EnvConfigFactory
 
 app = Flask(__name__)
+
+config: Config = EnvConfigFactory.create()
+app.config.from_object(config)
 
 from app import routes
