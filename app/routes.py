773451656916d42d0ff99863c37419dbb35e3e32
Farooq Mahmud <farooq@teqniqly.com>
2020-05-08 19:46:47 -0700

    User login

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,54 @@
+from werkzeug.urls import url_parse
+
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms.loginform import LoginForm
+from app.models import User
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    params = dict(
-        title="Welcome",
-        user={"username": "farooqam"},
-        posts=[
-            dict(author={"username": "Bubba"}, body="Python is cool!"),
-            dict(author={"username": "Felix"}, body="Mother's Day gift ideas?"),
-        ],
-    )
+    posts = [
+        {"author": {"username": "farooqam"}, "body": "Beautiful day in Seattle!"},
+        {"author": {"username": "engreen"}, "body": "Bombshell movie was so cool!"},
+    ]
+
+    params = dict(title="Welcome", posts=posts)
 
     return render_template("index.jinja2", **params)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("/index"))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash(
-            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
-        )
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if not user or not user.check_password(form.password.data):
+            flash("Invalid username and/or password.")
+            return redirect(url_for("login"))
+
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get("next")
+
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+
+        return redirect(next_page)
 
     params = dict(title="Sign In", form=form)
     return render_template("login.jinja2", **params)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
