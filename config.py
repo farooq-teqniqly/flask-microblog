74e3bf1cd375f38cb7b66c5843b094db92cf0282
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-05-08 18:55:35 -0700

    SQLAlchemy/SQLite integration

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 import os
+from typing import Any, Dict
+
 from dotenv import load_dotenv
 
 
 class Config(object):
-    def __init__(self, secret_key: str):
+    def __init__(
+        self,
+        secret_key: str,
+        sqlalchemy_database_uri: str,
+        sqlalchemy_track_modifications: bool,
+    ):
         self.SECRET_KEY = secret_key
+        self.SQLALCHEMY_DATABASE_URI = sqlalchemy_database_uri
+        self.SQLALCHEMY_TRACK_MODIFICATIONS = sqlalchemy_track_modifications
 
 
 class EnvConfigFactory:
@@ -12,8 +21,10 @@ class EnvConfigFactory:
     def create() -> Config:
         load_dotenv()
         secret_key = os.getenv("SECRET_KEY")
+        db_uri = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db', 'app.db')}"
+        db_track_modifications = False
 
         if not secret_key:
             raise ValueError("SECRET_KEY must be set!")
 
-        return Config(secret_key)
+        return Config(secret_key, db_uri, db_track_modifications)
--- microblog.py
+++ microblog.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, db
+import uuid
+from app.models import User, Post
 
 if __name__ == "__main__":
     app.run()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Post=Post, uuid=uuid)
