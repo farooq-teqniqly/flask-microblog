0741bedd767a31144e81814e58b1ed0cd702c9c4
Farooq Mahmud <farooq@teqniqly.com>
2020-05-10 17:39:43 -0700

    Profile page.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -22,12 +23,18 @@ class User(UserMixin, db.Model):
         )
 
         return f"<User> {d}"
+        return f"<User> {d}"
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_gravatar(self, size=80):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -84,3 +84,15 @@ def register():
 
     params = dict(title="Register", form=form)
     return render_template("userreg.jinja2", **params)
+
+
+@app.route("/user/<username>")
+def user(username: str):
+    user = User.query.filter_by(username=username).first_or_404()
+
+    posts = [
+        {"author": user, "body": "Beautiful day in Seattle!"},
+        {"author": user, "body": "Bombshell movie was so cool!"},
+    ]
+
+    return render_template("user.jinja2", user=user, posts=posts)
